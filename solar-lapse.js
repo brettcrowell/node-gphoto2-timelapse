d59b6cc6b001fa09d4904352d3040ebe55ba9172
bcrowell <brettcrowell@gmail.com>
2015-03-07 18:26:44 -0500

    prototyping

--- solar-lapse.js
+++ solar-lapse.js
@@ -1,179 +1,201 @@
-var gphoto2, aws, fs, usb, winston, simplex;
+var Timelapse = function(s3bucket){
 
-// prepare global requirements
-aws = require('aws-sdk');
-fs = require('fs');
-winston = require('winston');
-simplex = require('./clock.js');
+  var now = new Date().getTime();
+  
+  this.libs = {
 
-/**
- * Something bad happened.  Ask the operating system to
- * reset all USB connections to see if we can fix it.
- */
-
-function resetUsb(reason, callback){
-  camera = null;
-  gphoto2 = null;
-  winston.error(reason + ': rebooting');
-
-  usb = require('usb');
-
-  var c = usb.findByIds(1200, 810);
-
-  if(c){
-
-    // try to reset the usb connection
-    c.open();
-    c.reset(callback);
-
-  } else {
-
-    // crash if we can't get going again
-    process.exit(1);
-
+    tgphoto2: undefined,
+    aws: require('aws-sdk'),
+    fs: require('fs'),
+    winston: require('winston'),
+    simplex: require('./clock.js')
+    
   }
 
-}
+  this.camera = undefined;
+  this.exposures = [];
+  this.bucketName = s3bucket || now;
 
-/**
- * Use node-gphoto2 to capture an image from the active camera
- * and upload to Amazon S3
- *
- * @param camera  An instance of a GPhoto2 camera
- * @param imageProps Image metadata including name and timestamp (ts)
- */
-
-function takePicture(imageProps){
-
-  // keep a callback in case something goes wrong
-  var callback = function(){
-    takePicture(imageProps);
-  };
-
-  if(!camera){
-
-    gphoto2 = require('gphoto2');
-    var GPhoto = new gphoto2.GPhoto2();
-
-    // List cameras / assign list item to variable to use below options
-    GPhoto.list(function (list) {
-
-      if (list.length === 0) {
-        // no cameras found?  not buying it.
-        resetUsb('no cameras found', callback);
-        return;
-      }
-
-      camera = list[0];
-
-      winston.info('Found', camera.model);
-
-      // take the picture as a callback
-      callback();
-
-    });
-
-    return;
-
+  if (!this.libs.fs.existsSync('logs')){
+    this.libs.fs.mkdirSync('logs');
   }
 
-  camera.takePicture({download: true}, function (er, data) {
+  this.libs.winston.add(this.libs.winston.transports.File, { filename: 'logs/' + now + '.log' });
 
-    var imageFilename = imageProps.name + imageProps.ts + '.jpg',
-        imageDirectory = __dirname + '/output',
-        imagePath = imageDirectory + '/' + imageFilename;
+  this.libs.winston.info('solar lapse is up and running at ' + this.bucketName);
 
-    if (!fs.existsSync(imageDirectory)){
-      fs.mkdirSync(imageDirectory);
-    }
+  // fake two shots
+  this.exposures = new this.libs.simplex.Clock(now);
 
-    fs.writeFile(imagePath, data, function (err) {
-
-      if (err){
-
-        //resetUsb('error writing data to disk');
-
-      } else {
-
-        var fileSizeInBytes = fs.statSync(imagePath)["size"],
-            fileSizeInMegabytes = fileSizeInBytes / 1000000.0
-
-        winston.info('Size of ' + imageFilename + ': ' + fileSizeInMegabytes + 'mb');
-
-        if(fileSizeInBytes < 100000){
-          resetUsb('insufficient filesize detected', callback);
-        }
-
-        var imageStream = fs.createReadStream(imagePath);
-
-        var s3 = new aws.S3({
-          params: {
-            Bucket: 'bc-timelapse',
-            Key: imageProps.bucket + "/" + imageFilename
-          }
-        });
-
-        s3.upload({ Body: imageStream}, function(err, data) {
-          if (err) {
-            winston.info("Error uploading data: ", err);
-          } else {
-            // only delete data if we're sure we've uploaded it to s3
-            fs.unlink(imagePath);
-          }
-        });
-
-      }
-
-    });
-
-  });
+  // @todo: future support for separting photos into buckets dynamically
+  this.takeNextPicture({ name: 'test', bucket: this.bucketName, ts: now });
 
 };
 
-/**
- * Recursive function which reads a list of exposure times
- * (in ms) and calls for a photo to be taken at each
- *
- * @param nextImage Image properties, must include name & timestamp (ts)
- */
+Timelapse.prototype = {
 
-var takeNextPicture = function(nextImage){
+  /**
+   * Something bad happened.  Ask the operating system to
+   * reset all USB connections to see if we can fix it.
+   */
 
-  winston.info('taking image ' + nextImage.name + ' (' + nextImage.ts + ')');
+  resetUsb: function(reason, callback){
+    
+    this.camera = null;
+    this.libs.gphoto2 = null;
+    
+    this.libs.winston.error(reason + ': rebooting');
 
-  takePicture(nextImage);
+    var usb = require('usb'),
+        c = usb.findByIds(1200, 810);
 
-  if(exposures.hasMoreImages()){
+    if(c){
 
-    var currentTime = new Date().getTime(),
-        nextImage = exposures.getNextImage();
+      // try to reset the usb connection
+      c.open();
+      c.reset(callback);
 
-    setTimeout(function(){
-      takeNextPicture(nextImage);
-    }, nextImage.ts - currentTime);
+    } else {
 
-    return;
+      // crash if we can't get going again
+      process.exit(1);
 
-  }
+    }
 
-  winston.info('done');
+  },
 
-}
+  /**
+   * Use node-this.libs.gphoto2 to capture an image from the active camera
+   * and upload to Amazon S3
+   *
+   * @param camera  An instance of a this.libs.gphoto2 camera
+   * @param imageProps Image metadata including name and timestamp (ts)
+   */
 
-// 'local' variables
-var camera, exposures;
+  takePicture: function(imageProps){
 
-var now = new Date().getTime();
+    var self = this;
 
-if (!fs.existsSync('logs')){
-  fs.mkdirSync('logs');
-}
+    // keep a callback in case something goes wrong
+    var callback = function(){
+      this.takePicture(imageProps);
+    }.bind(self);
 
-winston.add(winston.transports.File, { filename: 'logs/' + now + '.log' });
+    if(!this.camera){
 
-winston.info('solar lapse is up and running at ' + now);
+      this.libs.gphoto2 = require('gphoto2');
+      var GPhoto = new this.libs.gphoto2.GPhoto2();
 
-// fake two shots
-exposures = new simplex.Clock(now);
+      // List cameras / assign list item to variable to use below options
+      GPhoto.list(function (list) {
 
-takeNextPicture({ name: 'test', bucket: now, ts: now });
+        if (list.length === 0) {
+          // no cameras found?  not buying it.
+          self.resetUsb('no cameras found', callback);
+          return;
+        }
+
+        self.camera = list[0];
+
+        self.libs.winston.info('Found', self.camera.model);
+
+        // take the picture as a callback
+        callback();
+
+      });
+
+      return;
+
+    }
+
+    this.camera.takePicture({download: true}, function (er, data) {
+
+      var imageFilename = imageProps.name + imageProps.ts + '.jpg',
+          imageDirectory = __dirname + '/output',
+          imagePath = imageDirectory + '/' + imageFilename;
+
+      if (!self.libs.fs.existsSync(imageDirectory)){
+        self.libs.fs.mkdirSync(imageDirectory);
+      }
+
+      self.libs.fs.writeFile(imagePath, data, function (err) {
+
+        if (err){
+
+          //resetUsb('error writing data to disk');
+
+        } else {
+
+          var fileSizeInBytes = self.libs.fs.statSync(imagePath)["size"],
+              fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+
+          self.libs.winston.info('Size of ' + imageFilename + ': ' + fileSizeInMegabytes + 'mb');
+
+          if(fileSizeInBytes < 100000){
+            self.resetUsb('insufficient filesize detected', callback);
+          }
+
+          var imageStream = self.libs.fs.createReadStream(imagePath);
+
+          var s3 = new self.libs.aws.S3({
+            params: {
+              Bucket: 'bc-timelapse',
+              Key: imageProps.bucket + "/" + imageFilename
+            }
+          });
+
+          s3.upload({ Body: imageStream}, function(err, data) {
+            if (err) {
+              self.libs.winston.info("Error uploading data: ", err);
+            } else {
+              // only delete data if we're sure we've uploaded it to s3
+              self.libs.fs.unlink(imagePath);
+            }
+          });
+
+        }
+
+      });
+
+    });
+
+  },
+
+  /**
+   * Recursive function which reads a list of exposure times
+   * (in ms) and calls for a photo to be taken at each
+   *
+   * @param nextImage Image properties, must include name & timestamp (ts)
+   */
+
+   takeNextPicture: function(nextImage){
+
+      var self = this;
+
+      this.libs.winston.info('taking image ' + nextImage.name + ' (' + nextImage.ts + ')');
+
+      this.takePicture(nextImage);
+
+      if(this.exposures.hasMoreImages()){
+
+        var currentTime = new Date().getTime(),
+            nextImage = this.exposures.getNextImage();
+
+        setTimeout(function(){
+
+          // wait (diff now and next exposure) then recurse
+          self.takeNextPicture(nextImage);
+
+        }, nextImage.ts - currentTime);
+
+        return;
+
+      }
+
+      this.libs.winston.info('done');
+
+    }
+
+};
+
+new Timelapse();
