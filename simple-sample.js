06099ca1fe0895ea40c3ff9f58054fdf02a1f1db
bcrowell <brettcrowell@gmail.com>
2015-07-30 00:18:28 -0400

    fixing simple-sample

--- simple-sample.js
+++ simple-sample.js
@@ -4,17 +4,17 @@ tl = require('./timelapse.js');
 seq = require('./sequence.js');
 
 var now = new Date().getTime(),
-    exposures = [];
+    exposureTimes = [];
 
 // take 30 exposures, one every 30 seconds, starting immediatey
 
 for(var i = 0; i < 30; i++){
   exposureTimes.push({
     name: 'test-lapse',
-    now + (i * 30000)
+    ts: now + (i * 30000)
   });
 }
 
-var myTestSequence = new seq.Sequence(exposures);
+var myTestSequence = new seq.Sequence(exposureTimes);
 
 new tl.Timelapse(myTestSequence);
