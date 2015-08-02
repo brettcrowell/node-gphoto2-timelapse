76232363b24ec8fbf0e48eb82ece3bb5a87f0671
bcrowell <brettcrowell@gmail.com>
2015-08-01 22:01:53 -0400

    adding another sample

---
+++ guided-sample.js
@@ -0,0 +1,56 @@
+var tl, seq, winston;
+
+tl = require('./timelapse.js');
+seq = require('./sequence.js');
+winston = require('winston');
+
+var now = new Date().getTime(),
+    myTestSequence = new seq.Sequence();
+
+var params = {
+
+  name: "guided-sample-lapse",
+
+  input: {
+
+    // starting now, how long would you like to lapse for (real-time)?
+
+    days: 0,
+    hours: 3,
+    minutes: 30,
+    seconds: 0
+
+  },
+
+  output: {
+
+    // how long would you like your output to be?
+
+    minutes: 0,
+    seconds: 45,
+    frameRate: 30
+
+  }
+
+};
+
+// calculate lapse properties based on preferences @todo integrate moment.js!
+var numFrames = ((params.output.minutes * 60) + params.output.seconds) * params.frameRate,
+    secondsToLapse = (params.input.days * 24 * 60 * 60) + (params.input.hours * 60 * 60) + (params.minutes * 60) + params.seconds,
+    frameInterval = secondsToLapse / numFrames;
+
+if(frameInterval < 10){
+  winston.warn("some cameras/configurations may have trouble capturing more frequently than every 10 seconds.  be careful!")
+}
+
+for(var i = 0; i < numFrames; i++){
+
+  myTestSequence.addImage({
+
+    name: params.name,
+    ts: now + (i * (frameInterval * 1000))
+
+  });
+}
+
+new tl.Timelapse(myTestSequence);
