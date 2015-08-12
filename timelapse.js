89e2cff1a2c6596a94423c7a65d07a03d6d9d31b
bcrowell <brettcrowell@gmail.com>
2015-08-12 19:55:01 -0400

    removing ugly libs object

--- timelapse.js
+++ timelapse.js
@@ -4,29 +4,24 @@
  * @constructor
  */
 
+var gphoto2 = undefined;
+var aws = require('aws-sdk');
+var fs = require('fs');
+var winston = require('winston');
+var seq = require('./sequence.js');
+var moment = require('moment');
+var exec = require('child_process').exec;
+
 var Timelapse = function(exposureSeq, preferences){
 
   var now = new Date().getTime();
-
-  //@todo find out how other node developers do this!!
-  this.libs = {
-
-    gphoto2: undefined,
-    aws: require('aws-sdk'),
-    fs: require('fs'),
-    winston: require('winston'),
-    seq: require('./sequence.js'),
-    moment: require('moment'),
-    exec: require('child_process').exec
-    
-  }
 
   // preferences
   this.preferences = preferences || {};
   this.preferences["maxMillisecondsBetweenImages"] = this.preferences["maxMillisecondsBetweenImages"] || 3600000;
 
   // moment.js configuration
-  this.libs.moment.locale('en', {
+  moment.locale('en', {
     calendar : {
       lastDay : '[yesterday at] LTS',
       sameDay : '[today] (L) [at] LTS',
@@ -40,21 +35,21 @@ var Timelapse = function(exposureSeq, preferences){
   this.camera = undefined;
   this.usbPath = undefined;
   this.deferredImage = undefined;
-  this.sequence = (exposureSeq.length > 0) ? new this.libs.seq.Sequence(exposureSeq) : exposureSeq;
+  this.sequence = (exposureSeq.length > 0) ? new seq.Sequence(exposureSeq) : exposureSeq;
 
-  if (!this.libs.fs.existsSync('logs')){
-    this.libs.fs.mkdirSync('logs');
+  if (!fs.existsSync('logs')){
+    fs.mkdirSync('logs');
   }
 
-  this.libs.winston.exitOnError = false;
-  this.libs.winston.add(this.libs.winston.transports.File, { filename: 'logs/' + now + '.log' });
-  this.libs.winston.info('solar lapse is up and running at ' + this.libs.moment(now).format('LTS [on] L'));
+  winston.exitOnError = false;
+  winston.add(winston.transports.File, { filename: 'logs/' + now + '.log' });
+  winston.info('solar lapse is up and running at ' + moment(now).format('LTS [on] L'));
 
   /**
    * swallowing all errors this way is a bad idea, @todo: please revisit!
    */
   process.on('uncaughtException', function (err) {
-    this.libs.winston.warn('Caught uncaught exception: ' + err);
+    winston.warn('Caught uncaught exception: ' + err);
   }.bind(this));
 
   // @todo: future support for separting photos into buckets dynamically
@@ -76,25 +71,25 @@ Timelapse.prototype = {
 
     var self = this;
 
-    self.libs.winston.info("resetting usb port because " + reason);
+    winston.info("resetting usb port because " + reason);
 
     if(this.camera){
 
       // clear out old data so callback will trigger correct phase
       this.camera = null;
-      this.libs.gphoto2 = null;
+      gphoto2 = null;
 
-      this.libs.winston.warn('re-establishing connection to ' + self.usbPath);
+      winston.warn('re-establishing connection to ' + self.usbPath);
 
-      this.libs.exec('./usbreset ' + self.usbPath, function(err, stdout, stderr){
+      exec('./usbreset ' + self.usbPath, function(err, stdout, stderr){
 
-        self.libs.winston.info('stdout: ' + stdout);
-        self.libs.winston.info('stderr: ' + stderr);
+        winston.info('stdout: ' + stdout);
+        winston.info('stderr: ' + stderr);
 
         if (err !== null) {
 
           // crash if we can't get going again
-          self.libs.winston.error('exec error: ' + err);
+          winston.error('exec error: ' + err);
 
         }
 
@@ -104,7 +99,7 @@ Timelapse.prototype = {
 
     } else {
 
-      self.libs.winston.error('unable to locate usable camera.  please check hardware');
+      winston.error('unable to locate usable camera.  please check hardware');
       process.exit(1);
 
     }
@@ -112,10 +107,10 @@ Timelapse.prototype = {
   },
 
   /**
-   * Use node-this.libs.gphoto2 to capture an image from the active camera
+   * Use node-gphoto2 to capture an image from the active camera
    * and upload to Amazon S3
    *
-   * @param camera  An instance of a this.libs.gphoto2 camera
+   * @param camera  An instance of a gphoto2 camera
    * @param imageProps Image metadata including name and timestamp (ts)
    */
 
@@ -136,8 +131,8 @@ Timelapse.prototype = {
         find a camera to use.  Since this is an async process, need to provide a callback.
        */
 
-      this.libs.gphoto2 = require('gphoto2');
-      var GPhoto = new this.libs.gphoto2.GPhoto2();
+      gphoto2 = require('gphoto2');
+      var GPhoto = new gphoto2.GPhoto2();
 
       // List cameras / assign list item to variable to use below options
       GPhoto.list(function (list) {
@@ -154,8 +149,8 @@ Timelapse.prototype = {
 	var port = self.camera.port.match(/usb:([0-9]+),([0-9]+)/);
         self.usbPath = '/dev/bus/usb/' + port[1] + '/' + port[2];
 
-        self.libs.winston.info('Found', self.camera.model);
-	self.libs.winston.info('Camera found on port ' + self.usbPath);
+        winston.info('Found', self.camera.model);
+	winston.info('Camera found on port ' + self.usbPath);
 
         // take the picture as a callback
         callback();
@@ -179,13 +174,13 @@ Timelapse.prototype = {
           imageDirectory = __dirname + '/output',
           imagePath = imageDirectory + '/' + imageFilename;
 
-      if (!self.libs.fs.existsSync(imageDirectory)){
-        self.libs.fs.mkdirSync(imageDirectory);
+      if (!fs.existsSync(imageDirectory)){
+        fs.mkdirSync(imageDirectory);
       }
 
-      self.libs.winston.info('taking image ' + imageProps.name + ' (' + imageProps.ts + ')');
+      winston.info('taking image ' + imageProps.name + ' (' + imageProps.ts + ')');
 
-      self.libs.fs.writeFile(imagePath, data, function (err) {
+      fs.writeFile(imagePath, data, function (err) {
 
         if (err){
 
@@ -193,15 +188,15 @@ Timelapse.prototype = {
 
         } else {
 
-          var fileSizeInBytes = self.libs.fs.statSync(imagePath)["size"],
+          var fileSizeInBytes = fs.statSync(imagePath)["size"],
               fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
 
-          self.libs.winston.info('Size of ' + imageFilename + ': ' + fileSizeInMegabytes + 'mb');
+          winston.info('Size of ' + imageFilename + ': ' + fileSizeInMegabytes + 'mb');
 
           if(fileSizeInBytes < 100000){
 
             self.resetUsb('insufficient filesize detected', callback);
-            self.libs.fs.unlink(imagePath);
+            fs.unlink(imagePath);
             return;
 
           }
@@ -214,7 +209,7 @@ Timelapse.prototype = {
 
         var currentImageDelay = new Date().getTime() - imageProps.ts;
 
-        self.libs.winston.info('operating delay for current image was ' + (currentImageDelay / 1000) + "s")
+        winston.info('operating delay for current image was ' + (currentImageDelay / 1000) + "s")
 
         self.takeNextPicture(currentImageDelay);
 
@@ -229,9 +224,9 @@ Timelapse.prototype = {
     var recurse = recurse || 2,
         self = this;
 
-    var imageStream = self.libs.fs.createReadStream(imagePath);
+    var imageStream = fs.createReadStream(imagePath);
 
-    var s3 = new self.libs.aws.S3({
+    var s3 = new aws.S3({
       params: {
         Bucket: bucket,
         Key: key
@@ -242,7 +237,7 @@ Timelapse.prototype = {
 
       if (err) {
 
-        self.libs.winston.warn("error uploading data: ", err);
+        winston.warn("error uploading data: ", err);
 
         if(recurse > 0){
           self.uploadToS3(imagePath, bucket, key, recurse - 1);
@@ -250,10 +245,10 @@ Timelapse.prototype = {
 
       } else {
 
-        self.libs.winston.info(key + ' has uploaded successfully to S3')
+        winston.info(key + ' has uploaded successfully to S3')
 
         // only delete data if we're sure we've uploaded it to s3
-        self.libs.fs.unlink(imagePath);
+        fs.unlink(imagePath);
 
       }
 
@@ -310,7 +305,7 @@ Timelapse.prototype = {
 
         // due to built in delays, next image might not capture at specified ts.  keep the user informed!
         var nextImageActualTs = currentTime + millisecondsUntilNextImage;
-        this.libs.winston.info("next image (`" + nextImage.name + "`) will be taken " + this.libs.moment(nextImageActualTs).calendar());
+        winston.info("next image (`" + nextImage.name + "`) will be taken " + moment(nextImageActualTs).calendar());
 
         // use a javascript timeout to wait, then capture the next image
         setTimeout(function(){ this.takePicture(nextImage); }.bind(this), millisecondsUntilNextImage);
@@ -319,7 +314,7 @@ Timelapse.prototype = {
 
       }
 
-      this.libs.winston.info('done');
+      winston.info('done');
 
     }
 
